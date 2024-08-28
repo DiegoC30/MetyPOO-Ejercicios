ba8f2d2cc0c57d86638ebcec25d42e7d464f1480
Diego C <116970878+DiegoC30@users.noreply.github.com>
2024-08-27 20:59:21 -0600

    Add files via upload

---
+++ ejecicios/ejecicios/src/main/java/org/example/ParImpar.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        int numero= 8;
+        if (numero % 2==0) {
+            System.out.println("el numero es par");
+        } else
+            {
+                System.out.println("el numero es impar");
+        }
+    }
+}
